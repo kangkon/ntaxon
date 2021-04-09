45803b9203529b4ef64d190cc7d4fcca2e8ae482
IASST BIF <bioinformatics@iasst.res.in>
2021-04-09 15:26:13 +0530

    haplotype Screening

--- ntaxon/fingerprinting/rflp.py
+++ ntaxon/fingerprinting/rflp.py
@@ -171,7 +171,7 @@ class RestrictionDigestion:
         plt.show()
 
     def plot_electrophoretic_diagram(self, figsize=(16, 10), min_size=1, min_frequency=1, min_common_haplotye=1,
-                                     lane_order=None):
+                                     lane_order=None, size_tolerance=0):
         """
         Plot heatmap based on presence and absence of bands
 
@@ -180,13 +180,16 @@ class RestrictionDigestion:
         :param min_frequency: Int - Minimum frequency
         :param min_common_haplotye: Int - Filter by minimum common haplotypes
 
-        :param min_occurrence: Int, Filter by Minimum Occurrence
+        :param size_tolerance: Int, Group by certain fragment size tolerance
         """
         matrix_bin = self.__digestion_profile.get_binary_matrix()
 
         if min_size > 1 or min_frequency > 1 or min_common_haplotye > 1:
             matrix_bin = self.__construct_binary_matrix(min_size=min_size, min_frequency=min_frequency,
                                                         min_common_haplotye=min_common_haplotye).get_binary_matrix()
+        if size_tolerance > 0:
+            matrix_bin = self.apply_tolerance(size_tolerance=size_tolerance, matrix=matrix_bin)
+
         if lane_order:
             if len(lane_order) == matrix_bin.shape[1]:
                 matrix_bin = matrix_bin[lane_order]
@@ -197,6 +200,86 @@ class RestrictionDigestion:
         ax = sns.heatmap(matrix_bin, cmap="YlGnBu", cbar=False)
         plt.show()
 
-    def get_haplotypes(self, allow_size_difference=1, isolate_filter=None):
-        print('Alignment')
+    def apply_tolerance(self, size_tolerance=1, matrix=None):
+        matrix_bin = matrix if matrix is not None else self.__digestion_profile.get_binary_matrix()
+        fragments = []
+        for f in matrix_bin.index.values.to_list():
+            # get the size by spliting across _
+            splitted = f.split('_')
+            fragments.append([f, splitted[0]])
 
+        diff_matrix = []
+        for f, s in fragments:
+            dif = []
+            for _f, _s in fragments:
+                dif.append(abs(float(s) - float(_s)))  # Get absolute values only
+            diff_matrix.append(dif)
+
+        diff_matrix = np.tril(diff_matrix, -1)  # convert to lower triangular form
+        diff_matrix = pd.DataFrame(diff_matrix, columns=matrix_bin.index.values.to_list(),
+                                   index=matrix_bin.index.values.to_list())
+
+        diff_matrix_stacked = diff_matrix.stack().to_frame().reset_index()
+        diff_matrix_stacked.columns = ['frag_1', 'frag_2', 'difference']
+
+        # Filter by tolerance
+        tolerance_filtered = diff_matrix_stacked[diff_matrix_stacked['difference'] <= size_tolerance]
+        tolerance_filtered = tolerance_filtered[tolerance_filtered['difference'] != 0]
+
+        # Compute homogenity for a fragment
+        sample_count = matrix_bin.shape[1]  # no of samples
+        fragment_occurance_count = matrix_bin.sum(axis=1)
+
+        def compute_merge_vars(r):
+            # get homogenity scores (fragment count scores)
+            frag_1_hom = fragment_occurance_count.loc[r['frag_1']]
+            frag_2_hom = fragment_occurance_count.loc[r['frag_2']]
+
+            frag_sum = matrix_bin.loc[[r['frag_1'], r['frag_2']], :].sum(axis=0)
+            return pd.Series([
+                sample_count - frag_sum[frag_sum == 0].count(),
+                r['frag_1'] if frag_1_hom > frag_2_hom else r['frag_2']
+            ])
+
+        tolerance_filtered[['merged_frag_count', 'merge_to']] = tolerance_filtered.apply(compute_merge_vars, axis=1)
+        # Sort to merge fragments in correct order
+        tolerance_filtered = tolerance_filtered.sort_values(by='merged_frag_count')
+        # Construct the merged matrix
+        matrix_bin_merged = matrix_bin.reset_index()
+        for idx, r in tolerance_filtered.iterrows():
+            mergable_frag = r['frag_1'] if r['merge_to'] == r['frag_2'] else r['frag_2']
+            # mergable_frag_iloc = matrix_bin.index.get_loc(mergable_frag)
+            # matrix_bin_merged.iloc[mergable_frag_iloc, [0]] = r['merge_to']
+            matrix_bin_merged['haplotype'] = matrix_bin_merged['haplotype'].replace(mergable_frag, r['merge_to'])
+
+        # Create new fresh dataframe # otherwise groupby causes problem
+        df = pd.DataFrame(matrix_bin_merged.values.tolist(), columns=matrix_bin_merged.columns)
+        # Combine rows and replace value > 1 to 1
+        return df.groupby('haplotype').agg('sum').apply(lambda x: [1 if y > 0 else 0 for y in x])
+
+    def get_haplotypes(self, size_tolerance=0, matrix=None, exclude=None, allow_polymorphism=0):
+        matrix_bin = matrix if matrix is not None else self.__digestion_profile.get_binary_matrix()
+
+        if exclude is not None:
+            columns = list(matrix_bin.columns)
+
+            if type(exclude) == str:
+                if exclude not in columns:
+                    raise Exception(f"{exclude} does not exist")
+            elif type(exclude) == list:
+                for c in exclude:
+                    if c not in columns:
+                        raise Exception(f"{c} does not exist")
+            else:
+                raise Exception("Invalid Exclude Value")
+
+            matrix_bin.drop(exclude, axis=1)
+
+        if size_tolerance > 0:
+            matrix_bin = self.apply_tolerance(size_tolerance=size_tolerance, matrix=matrix_bin)
+
+        sample_count = matrix_bin.shape[1]
+
+        polymorphism_scores = 1 - matrix_bin.sum(axis=1) / sample_count
+        polymorphism_scores = polymorphism_scores.round(decimals=1)
+        return polymorphism_scores[polymorphism_scores <= allow_polymorphism].index.tolist()
